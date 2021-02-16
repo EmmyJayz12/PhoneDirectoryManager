03708dc20971ec5d4aed835990bbd62924598f90
Dominic <domebingonye@gmail.com>
2021-02-16 08:52:05 +0100

    initial setup.

---
+++ PhoneDirectoryManager/PhoneDirectoryManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhoneDirectoryManager.MvcApplication" Language="C#" %>
---
+++ PhoneDirectoryManager/PhoneDirectoryManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhoneDirectoryManager.Startup))]
+namespace PhoneDirectoryManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
